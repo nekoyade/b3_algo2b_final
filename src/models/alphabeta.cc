149007210c33f1208f7d0892a7b273975dbfb457
nekoyade <ktr.km.sch@gmail.com>
2024-07-28 21:53:55 +0900

    Create models/alphabeta

---
+++ src/models/alphabeta.cc
@@ -0,0 +1,14 @@
+/*  Project name    : A2BF
+ *  File name       : models/alphabeta.cc
+ *
+ *  2024 Keitaro Kamo (nekoyade)
+ *
+ */
+
+#include "models/alphabeta.h"
+
+namespace {
+
+// ...
+
+}  // namespace
---
+++ src/models/alphabeta.h
@@ -0,0 +1,24 @@
+/*  Project name    : A2BF
+ *  File name       : models/alphabeta.h
+ *
+ *  2024 Keitaro Kamo (nekoyade)
+ *
+ */
+
+#ifndef A2BF_MODELS_ALPHABETA_H_
+#define A2BF_MODELS_ALPHABETA_H_
+
+namespace {
+
+class AlphabetaSolver {
+public:
+    AlphabetaSolver() {}
+    ~AlphabetaSolver() {}
+
+private:
+    // ...
+};
+
+}  // namespace
+
+#endif  // A2BF_MODELS_ALPHABETA_H_
